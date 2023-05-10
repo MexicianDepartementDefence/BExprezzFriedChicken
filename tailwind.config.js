07cb72f72afd348652eaa2dd9e74d4086b698b12
MexicianDepartementDefense <faisal216093@gmail.com>
2023-05-10 12:43:32 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./Web Random/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage :
+      {
+        "BFC" : "url('../assett/img/3da37dc6421f978a50e165466f221d72.jpg')"
+      }
     },
   },
   plugins: [],
