ddf11cbda4e207806f3686709e3f21c306fef53f
MexicianDepartementDefense <faisal216093@gmail.com>
2023-05-11 14:52:53 +0700

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,8 @@ module.exports = {
   content: ["./Web Random/**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage :
-      {
-        "BFC" : "url('../assett/img/3da37dc6421f978a50e165466f221d72.jpg')"
+      backgroundImage : {
+        "BFC" : "url('../assett/img/—Pngtree—yellow gourmet shading cartoon chef_1025883.jpg')"
       }
     },
   },
